5590f1619f5b53ace1f8088f7a8aa670bab3fe51
David Kvaček <121750516+svaaaca@users.noreply.github.com>
2023-07-01 11:54:46 +0200

    Add files via upload

---
+++ 1-bit/zimni/izp/lab-09/main.c
@@ -0,0 +1,42 @@
+/**
+ * Kostra programu pro vypocet Fibonacciho cisel
+ * 2020, Ales Smrcka
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long int ull;
+
+/**
+ * Fib(n) =
+ *  0                   n=0
+ *  1                   n=1
+ *  Fib(n-1)+Fib(n-2)   n>1
+ *
+ * 1. Naprogramujte funkci rekurzivne.
+ * 2. Zjistete, kolikrat byla funkce volana.
+ * 3. Akcelerujte vypocet pomoci statickeho nebo globalniho pole vysledku.
+ */
+
+ull fib(ull n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char **argv)
+{
+    ull n;
+    if (argc < 2)
+        return 1;
+    else
+    	n = strtoul(argv[1], NULL, 10);
+
+    printf("fib(%llu) = %llu\n", n, fib(n));
+    return 0;
+}
+
