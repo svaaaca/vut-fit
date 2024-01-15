2bf2ace660339cca7dd1eef76ca8c735955ddc5e
David Kvaček <121750516+svaaaca@users.noreply.github.com>
2024-01-15 12:28:39 +0100

    Add files via upload

---
+++ 3-bit/zimni/imp/proj-1/debug/sources/main.d
@@ -0,0 +1,18 @@
+Sources/main.o: ../Sources/main.c ../Includes/MK60DZ10.h \
+ ../Includes/core_cm4.h ../Includes/core_cmInstr.h \
+ ../Includes/core_cmFunc.h ../Includes/core_cm4_simd.h \
+ ../Includes/system_MK60DZ10.h ../Includes/main.h
+
+../Includes/MK60DZ10.h:
+
+../Includes/core_cm4.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/core_cm4_simd.h:
+
+../Includes/system_MK60DZ10.h:
+
+../Includes/main.h:
