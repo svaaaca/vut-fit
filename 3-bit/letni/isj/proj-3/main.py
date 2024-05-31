eb2a809d3d11d7252e5031e43272c46386d0c467
David Kvaček <121750516+svaaaca@users.noreply.github.com>
2024-05-31 14:47:25 +0200

    Add files via upload

---
+++ 3-bit/letni/isj/proj-3/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# ukol za 2 body
+def first_odd_or_even(numbers):
+    """Returns 0 if there is the same number of even numbers and odd numbers
+       in the input list of ints, or there are only odd or only even numbers.
+       Returns the first odd number in the input list if the list has more even
+       numbers.
+       Returns the first even number in the input list if the list has more odd 
+       numbers.
+
+    >>> first_odd_or_even([2,4,2,3,6])
+    3
+    >>> first_odd_or_even([3,5,4])
+    4
+    >>> first_odd_or_even([2,4,3,5])
+    0
+    >>> first_odd_or_even([2,4])
+    0
+    >>> first_odd_or_even([3])
+    0
+    """
+
+    even = sum(1 for num in numbers if num % 2 == 0)
+    odd = len(numbers) - even
+
+    if even == odd or even == 0 or odd == 0:
+        return 0
+
+    if even > odd:
+        return next(num for num in numbers if num % 2 != 0)
+
+    return next(num for num in numbers if num % 2 == 0)
+
+# ukol za 3 body
+def to_pilot_alpha(word):
+    """Returns a list of pilot alpha codes corresponding to the input word
+
+    >>> to_pilot_alpha('Smrz')
+    ['Sierra', 'Mike', 'Romeo', 'Zulu']
+    """
+
+    pilot_alpha = ['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot',
+        'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike',
+        'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango',
+        'Uniform', 'Victor', 'Whiskey', 'Xray', 'Yankee', 'Zulu']
+
+    pilot_alpha_list = [pilot_alpha[ord(char.upper()) - ord('A')] for char in word if char.isalpha()]
+     
+    return pilot_alpha_list
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
